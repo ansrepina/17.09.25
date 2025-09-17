6217bc670d930d2dee9365f2fc7f1040300e16fa
User <User@DESKTOP-NC535I2>
2025-09-17 20:08:42 +0500

    Домашняя раабота 1

---
+++ homework-1/homework-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В осеннем парке — тишина," +
+                "\r\nЛиства танцует без конца." +
+                "\r\nИ каждый лист, как струна," +
+                "\r\nПоет о грусти для певца.");
+        }
+    }
+}
