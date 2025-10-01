d3309cd933a30d334808388530371114f5b970ed
User <User@DESKTOP-NC535I2>
2025-10-01 16:35:49 +0500

    Домашняя работа 1. Исправленная

--- homework-1/homework-1/Program.cs
+++ homework-1/homework-1/Program.cs
@@ -10,10 +10,20 @@ namespace homework_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("В осеннем парке — тишина," +
-                "\r\nЛиства танцует без конца." +
-                "\r\nИ каждый лист, как струна," +
-                "\r\nПоет о грусти для певца.");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Сергей Есенин");
+            Console.WriteLine("Живите так...");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Живите так,");
+            Console.WriteLine("Как вас ведет звезда");
+            Console.WriteLine("Под кущей обновленной сени");
+            Console.WriteLine("С приветствием,");
+            Console.WriteLine("Вас помнящий всегда");
+            Console.WriteLine("Знакомый ваш - Сергей Есенин.");
+            Console.ResetColor();
         }
     }
 }
