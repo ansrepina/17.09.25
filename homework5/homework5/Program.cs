26c7353a9d631a89456320a6262972ffaeaeb858
User <User@DESKTOP-NC535I2>
2025-10-08 20:29:41 +0500

    дз 5

---
+++ homework5/homework5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Calculate(2, 3) + Calculate(3, 7) + Calculate(5, 5);
+            Console.WriteLine(Math.Round(x, 3));
+        }
+
+        static double Calculate(double a, double b) =>
+            Math.Sqrt(a + Math.Log(1 + Math.Sqrt(b)));
+    }
+}
