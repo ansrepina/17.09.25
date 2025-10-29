ccc29684ee88cafda7931ea09e2884639d52cd0d
User <User@DESKTOP-NC535I2>
2025-10-29 18:12:41 +0500

    Задание 8

---
+++ homework8-1/homework8-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homework8_1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число k");
+            var k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите целое число m");
+            var m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите целое число n");
+            var n = int.Parse(Console.ReadLine());
+
+            if (AreSameParity(k, m, n))
+                Console.WriteLine("k, m, n - числа одинаковой четности");
+            else 
+                Console.WriteLine("k, m, n не являются числами одинаковой четности");
+        }
+
+        static bool AreSameParity(int k, int m, int n) =>
+            (k % 2 == m % 2) && (m % 2 == n % 2);
+    }
+}
---
+++ homework8-3/homework8-3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    static bool DecodePosition(string position, out int row, out int col)
+    {
+        row = position[1] - '1';
+        col = position[0] - 'a';
+        return true;
+    }
+    static bool IsBishopAttacking(int bishopRow, int bishopCol, int targetRow, int targetCol)
+    {
+        return Math.Abs(bishopRow - targetRow) == Math.Abs(bishopCol - targetCol);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите позицию белого слона (например, a1):");
+        string whitePosition = Console.ReadLine();
+
+        Console.WriteLine("Введите позицию черного слона (например, h8):");
+        string blackPosition = Console.ReadLine();
+
+        DecodePosition(whitePosition, out int whiteRow, out int whiteCol);
+        DecodePosition(blackPosition, out int blackRow, out int blackCol);
+
+        bool blackAttacksWhite = IsBishopAttacking(blackRow, blackCol, whiteRow, whiteCol);
+
+        bool whiteAttacksBlack = IsBishopAttacking(whiteRow, whiteCol, blackRow, blackCol);
+
+        if (blackAttacksWhite)
+            Console.WriteLine("Белый слон находится под боем черного слона.");
+        else
+            Console.WriteLine("Белый слон не находится под боем черного слона.");
+
+        if (whiteAttacksBlack)
+            Console.WriteLine("Черный слон находится под боем белого слона.");
+        else
+            Console.WriteLine("Черный слон не находится под боем белого слона.");
+
+        Console.ReadKey(); 
+    }
+}
+
+
