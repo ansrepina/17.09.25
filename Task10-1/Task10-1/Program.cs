53dc14273e353d1268e1b14a87ebfb701bcd13f6
User <User@DESKTOP-NC535I2>
2025-11-20 01:12:17 +0500

    Задание 10

---
+++ Task10-1/Task10-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение веса в фунтах:");
+
+            int n;
+
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return;
+            }
+
+            if (n == 0)
+            {
+                Console.WriteLine("Число не должно быть равно 0");
+                return;
+            }
+
+            const double gramsPerPound = 453;
+
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("        Фунты    |    Килограммы ");
+            Console.WriteLine("--------------------------------------------------");
+            for (int pounds = 1; pounds <= n; pounds++)
+            {
+
+                double totalGrams = pounds * gramsPerPound;
+                double kilograms = totalGrams / 1000;
+
+                Console.WriteLine($"{pounds,10} | {kilograms,16:F3}"); 
+            }
+
+        }
+    }
+}
---
+++ Task10-2/Task10-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            var n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1}-е число");
+                sum += double.Parse(Console.ReadLine());
+            }
+            double result = sum/n;
+            Console.WriteLine($"Среднее арифметическое чисел = {result}");
+        
+        }
+    }
+}
