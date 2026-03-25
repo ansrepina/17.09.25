6878cccccdaabf5865445de18a58499912398b7a
User <User@DESKTOP-NC535I2>
2026-03-25 12:36:34 +0500

    Задание 13

---
+++ MobileLibrary/MobileLibrary/Subscriber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileLibrary
+{
+    public class Subscriber
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public readonly string ContractNumber;
+
+        public string TariffName { get; set; }
+        public PaymentType PaymentKind { get; set; }
+        public decimal Balance { get; set; }
+
+        public Subscriber(string name, string surname, string phoneNumber, string contractNumber)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+            ContractNumber = contractNumber;
+
+            TariffName = "Базовый";
+            PaymentKind = PaymentType.Prepaid;
+            Balance = 0;
+        }
+
+        public virtual string[] GetInfo()
+        {
+            var info = new string[2];
+            info[0] = $"{Surname} {Name} (тел: {PhoneNumber})";
+            string pType = (PaymentKind == PaymentType.Prepaid) ? "Предоплата" : "В кредит";
+            info[1] = $"Договор: {ContractNumber}. Тариф: {TariffName}. Тип оплаты: {pType}. Баланс: {Balance} руб.";
+            return info;
+        }
+    }
+}
